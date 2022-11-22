f6fd1b94e456d1ebf9fc8965b44aceaeb8db3d06
herman <yauck.e.h@gmail.com>
2022-11-21 23:50:15 -0300

    add file

---
+++ peliculas.py
@@ -0,0 +1,10 @@
+#restriccines
+rest = ("G", "PG", "PG-13", "PG-16", "R")
+
+#descriccion de restricciones
+desc = {rest[0]:"audiencia genera",
+        rest[1]:"orientacion de los padres",
+        rest[2]:"orientación de los padres a menores de 13 años",
+        rest[3]:"orientación de los padres a menores de 16 años",
+        rest[4]:"acceso restringido a menores de edad"}
+
