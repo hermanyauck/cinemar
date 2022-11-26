d059cad756f34559ceae5d1dac0b1d24fc263952
hermanyauck <yauck.e.h@gmail.com>
2022-11-25 23:46:24 -0300

    Update user.py

--- user/user.py
+++ user/user.py
@@ -1,14 +1,21 @@
 import sqlite3 as base
 
-def addUser(nombre,edad,usuario,password):
-    conn = base.connect("base.db")
-    cursor = conn.cursor()
-    consulta = f'''INSERT INTO PERSONAS
-    ("nombre","edad","usuario","password")
-    VALUES 
-    ("{nombre}",{edad},"{usuario}","{password}");'''
-    cursor.execute(consulta)
-    conn.commit()
-    conn.close()
+puestos = ("client", "vendor", "admin")
 
-addUser("herman",27,"herman95","123")
+class add:
+    def agregar(nombre, edad, usuario, password, tipo):
+        conn = base.connect("base.db")
+        cursor = conn.cursor()
+        consulta = f'''INSERT INTO PERSONAS
+        ("nombre","edad","usuario","password","puesto")
+        VALUES 
+        ("{nombre}",{edad},"{usuario}","{password}","{tipo}");'''
+        cursor.execute(consulta)
+        conn.commit()
+        conn.close()
+
+    def client(nombre, edad, usuario, password):
+        add.agregar(nombre, edad, usuario, password, puestos[0])
+
+
+add.client("teodoro","5","teo","123")
